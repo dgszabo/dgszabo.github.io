3656217c46db8d7f51dbe4d69a932614429e6aa6
Daniel Szabo <dgszabo@gmail.com>
2018-05-20 21:01:35 -0700

    Own webpage reworked

---
+++ resources/js/app.js
@@ -0,0 +1,33 @@
+$(document).ready(() => {
+    let inTransition = false;
+    $('.load-cover').fadeOut();
+    
+    $('.nav-panel').on('click', 'a', (event) => {
+        event.preventDefault();
+        let pages = [ 'home', 'about', 'portfolio', 'resume', 'contact' ]
+        if(!inTransition && $(`.${event.target.id.split('-')[0]}-page`).hasClass('left-screen')) {
+            inTransition = true;
+            pages.forEach((page) => {
+                if(page !== `${event.target.id.split('-')[0]}`) {
+                    $(`.${page}-page`).removeClass('front').addClass('back');
+                    $(`#${page}-btn`).removeClass('slided-right');
+                }
+            });
+
+            $(`.${event.target.id.split('-')[0]}-page`).removeClass('hidden').addClass('visible');
+            setTimeout(() => {
+                $(`.${event.target.id.split('-')[0]}-page`).removeClass('left-screen back').addClass('front');
+                $(`#${event.target.id.split('-')[0]}-btn`).addClass('slided-right');
+            }, 20);
+
+            setTimeout(() => {
+                pages.forEach((page) => {
+                    if(page !== `${event.target.id.split('-')[0]}`) {
+                        $(`.${page}-page`).addClass('left-screen hidden');
+                    }
+                    inTransition = false;
+                });
+            }, 1020);
+        }
+    });
+});
